64b1043aba22ee75fa67bf64fda5075f07d7b29a
fastmed <maks.memelige@gmail.com>
2022-02-17 14:09:26 +0200

    Add CreateUserSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -9,3 +9,12 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
+
+
+class CreateUserSerializer(DjoserUserCreateSerializer):
+
+    class Meta:
+        model = User
+        exclude = ("last_login", "date_joined",
+                   "is_superuser", "is_staff", "is_active", 
+                   "groups", "user_permissions")
