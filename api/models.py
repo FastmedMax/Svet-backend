1f8155f895f40adbaed102b81b61e13c9f5c33d4
fastmed <maks.memelige@gmail.com>
2022-02-17 14:01:44 +0200

    Add Category model

--- api/models.py
+++ api/models.py
@@ -6,3 +6,10 @@ from django.contrib.auth.models import AbstractUser
 from .managers import UserManager
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(verbose_name="Название категории", max_length=60)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
