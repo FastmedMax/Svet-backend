8520835a2404ae40ca373479c79f91b7e8e3e7fa
fastmed <maks.memelige@gmail.com>
2022-02-17 14:09:14 +0200

    Add CategorySerializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,3 +3,9 @@ from rest_framework import serializers
 from djoser.serializers import UserCreateSerializer as DjoserUserCreateSerializer
 
 from .models import Category, Course, User
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
