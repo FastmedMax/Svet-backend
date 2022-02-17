356a170381fe35bc71d80af9052218f9fff1ab55
fastmed <maks.memelige@gmail.com>
2022-02-17 14:10:56 +0200

    Add imports for views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.generics import ListAPIView
+
+from .models import Category, Course
+from .serializers import CategorySerializer, CourseDetailSerializer, CuorseSerializer
 
 # Create your views here.
