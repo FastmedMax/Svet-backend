7c9410febcdb1bf13eae1f0972733efc8fd1d64d
fastmed <maks.memelige@gmail.com>
2022-02-17 14:11:08 +0200

    Add CategoryListView

--- api/views.py
+++ api/views.py
@@ -7,3 +7,14 @@ from .models import Category, Course
 from .serializers import CategorySerializer, CourseDetailSerializer, CuorseSerializer
 
 # Create your views here.
+class CategoryListView(ListAPIView):
+    queryset = Category
+    serializer_class = CategorySerializer
+
+    def get(self, request):
+        try:
+            categories = self.queryset.objects.all()
+        except Category.DoesNotExist:
+            return Response("Категории не найдены!", status=status.HTTP_404_NOT_FOUND)
+        serializer = self.serializer_class(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
